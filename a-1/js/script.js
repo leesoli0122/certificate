692e7b48c3e7c8dca582eed592ddc3a5ccabf22d
leesoli0122 <leesoli0122@gmail.com>
2026-08-14 09:38:24 +0900

    메뉴 변경 중

--- a-1/js/script.js
+++ a-1/js/script.js
@@ -1,19 +1,4 @@
 $(document).ready(function() {
     //메뉴
-    var $nav = $('header nav');
-    var $subMenu = $('.sub-menu');
 
-    $subMenu.hide();
-
-    $nav.on('mouseenter focusin', function() {
-        $subMenu.stop(true, true).slideDown(300);
-    });
-
-    $nav.on('mouseleave focusout', function(event) {
-        if (event.type === 'focusout' && $.contains(this, event.relatedTarget)) {
-            return;
-        }
-
-        $subMenu.stop(true, true).slideUp(300);
-    });
 });
